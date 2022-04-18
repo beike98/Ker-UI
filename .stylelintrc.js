cc4dfa71a9b00d4719b2721c4339b635c10d737a
baker <liubeike99@gmail.com>
2022-04-18 23:20:37 +0800

    chore

--- .stylelintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  "extends": [
-    "stylelint-config-standard",
-    "stylelint-config-standard-scss",
-    "stylelint-config-recommended-vue",
-    "stylelint-config-html/vue"
-  ]
-}
